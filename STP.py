1bd2dd61238adc37c22b03114c39f91f5ebbdd90
Hadi Eskandari <h.eskandari@gmail.com>
2020-12-01 15:24:05 +1100

    Better error handling

--- STP.py
+++ STP.py
@@ -3,77 +3,68 @@ from biplist import *
 import json
 import os
 import re
-
+import commands
+import subprocess
 
 class SourceTree:
-    homePath = os.path.expanduser('~')
-    filePath = homePath + "/Library/Application Support/SourceTree/browser.plist"
 
-    def _camel_case_split(self, title):
-        result = []
-        pattern = '.[^A-Z]*'
-        matches = re.finditer(pattern, title)
-        regex_parts = [m.group(0) for m in matches]
-        for part in regex_parts:
-            result.append(part.replace(".", ""))
+  homePath=os.path.expanduser('~')
+  filePath= homePath + "/Library/Application Support/SourceTree/browser.plist"
 
-        return result
+  def _camel_case_split(self, identifier):
+    matches = re.finditer('.+?(?:(?<=[a-z])(?=[A-Z])|(?<=[A-Z])(?=[A-Z][a-z])|$)', identifier)
+    return [m.group(0) for m in matches]
 
-    def _split_match_words(self, title):
-        res = set()
-        res.add(title)
+  def _splitMatchWords(self, title):
+    res = []
+    cam = self._camel_case_split(title)
+    for m in cam:
+      ret = re.split('-|_| |',m)
+      for n in ret:
+        res.append(n)
+    return ' '.join(res);
 
-        dot_parts = self._split_by_dot(title)
-        for p in dot_parts:
-            res.add(p)
+  def _getProjects(self):
+    try:
+      plist = readPlist(self.filePath)
+      tempName = ""
+      res = []
+      for item in plist['$objects']:
+        if(type(item) is str and item[:1] == '/'):
+          res.append([tempName, item])
+        elif (type(item) is str):
+          tempName = item
+      return res
+    except e:
+      return []
 
-        cam = self._camel_case_split(title)
-        for m in cam:
-            ret = re.split('-|_| |.|', m)
-            for n in ret:
-                res.add(n)
-        return ' '.join(res)
+  def getList(self):
+    items = []
+    projects = self._getProjects()
+    for p in projects:
+      item = {
+        'title': p[0],
+        'subtitle': p[1],
+        'arg': p[1],
+        'match': self._splitMatchWords(p[0])
+      }
+      items.append(item)
+    result = {'items': items}
+    print(json.dumps(result))
 
-    def _split_by_dot(self, title):
-        return title.split(".")
+  def cloneRepo(self, url):
+    parts = url.split('/')
+    project_name = parts[-1].replace('.git', '')
+    projects_path = self.homePath + "/Projects/"
+    new_project_path = projects_path + project_name
 
-    def _get_projects(self):
-        try:
-            plist = readPlist(self.filePath)
-            temp_name = ""
-            res = []
-            for item in plist['$objects']:
-                if type(item) is str and item[:1] == '/':
-                    res.append([temp_name, item])
-                elif type(item) is str:
-                    temp_name = item
-            return res
-        except:
-            return []
+    if os.path.isdir(new_project_path):
+      print('Directory already exists.')
 
-    def get_suggestions(self):
-        items = []
-        projects = self._get_projects()
-        for p in projects:
-            item = {
-                'title': p[0],
-                'subtitle': p[1],
-                'arg': p[1],
-                'match': self._split_match_words(p[0])
-            }
-            items.append(item)
-        result = {'items': items}
-        print(json.dumps(result))
-
-    def clone_repo(self, url):
-        parts = url.split('/')
-        project_name = parts[-1].replace('.git', '')
-        projects_path = self.homePath + "/Projects/"
-        new_project_path = projects_path + project_name
-
-        if os.path.isdir(new_project_path):
-            print('Directory already exists.')
-
-        os.chdir(projects_path)
-        os.system('git clone ' + url)
-        print(project_name + ' cloned successfully!')
+    try:
+      output = subprocess.check_output(['git clone ' + url + ' ' + new_project_path], shell=True, stderr=subprocess.STDOUT)
+      print(output.strip().splitlines()[-1])
+    except subprocess.CalledProcessError as ge:
+      print ge.output
+    except Exception as e:
+      print e
